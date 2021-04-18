dd36d96c545766ac34d4d6aec893d8a21f204176
Della Lin <della.lin222@gmail.com>
2021-04-18 16:29:49 -0700

    adding profile page

--- frontend_ver2/src/App.js
+++ frontend_ver2/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Profile from './components/pages/Profile';
+import ShareMusic from './components/pages/ShareMusic';
+import Discover from './components/pages/Discover';
+import Notifications from './components/pages/Notifications';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Navbar />
         <Switch>
           <Route path='/' exact/>
+          <Route path='/profile' component={Profile} />
+          <Route path='/share-music' component={ShareMusic} />
+          <Route path='/discover' component={Discover} />
+          <Route path='/notifications' component={Notifications} />
         </Switch>
     </Router>
     </>
