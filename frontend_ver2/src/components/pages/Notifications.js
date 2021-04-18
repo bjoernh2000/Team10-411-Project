c779252b485dcdfaead846ce5756369b6e512de4
Andrew Chung <andrewtc4@gmail.com>
2021-04-18 19:52:50 -0400

    Port Notifications page
    I was working off of Bernard's code, but Della's looks like the way
    we're going to go from here on out.

--- frontend_ver2/src/components/pages/Notifications.js
+++ frontend_ver2/src/components/pages/Notifications.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
-function Notifications() {
+import React from 'react';
+import './Notifications.css';
+export default function Notifications() {
     return (
-        <div>
-            
+        <>
+        <header>
+            <h1>BAD DJ ­-  Notifications</h1>
+        </header>
+        <div class="main">
+            <table>
+                <div class="notif-odd-row">
+                    <tr>
+                        <td class="notif-info">Adam has sent you a friend
+                                request.</td>
+                        <td><a href="#"><div class="button-link">Accept</div></a></td>
+                        <td><a href="#"><div class="button-link">Reject</div></a></td>
+                    </tr>
+                </div>
+                <div class="notif-even-row">
+                    <tr>
+                        <td class="notif-info">Della has commented on your playlist.</td>
+                        <td><a href="#"><div class="button-link">View</div></a></td>
+                        <td><a href="#"><div class="button-link">Ignore</div></a></td>
+                    </tr>
+                </div>
+            </table>
         </div>
-    )
+        </>
+    );
 }
-
-export default Notifications
