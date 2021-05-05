7d8a8e381b82a422d62adaa7dac7753ef9f59bc3
bmulaw <67084560+bmulaw@users.noreply.github.com>
2021-05-04 19:20:34 -0700

    worked on notifications.js to get data

--- frontend_ver2/src/components/pages/Notifications.js
+++ frontend_ver2/src/components/pages/Notifications.js
@@ -1,30 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { axios, backend_url } from '../../App.js';
 import './Notifications.css';
-export default function Notifications() {
-    return (
-        <>
-        <header>
-            <h1>BAD DJ ­-  Notifications</h1>
-        </header>
-        <div class="main">
-            <table>
-                <div class="notif-odd-row">
-                    <tr>
-                        <td class="notif-info">Adam has sent you a friend
-                                request.</td>
-                        <td><a href="#"><div class="button-link">Accept</div></a></td>
-                        <td><a href="#"><div class="button-link">Reject</div></a></td>
-                    </tr>
-                </div>
-                <div class="notif-even-row">
-                    <tr>
-                        <td class="notif-info">Della has commented on your playlist.</td>
-                        <td><a href="#"><div class="button-link">View</div></a></td>
-                        <td><a href="#"><div class="button-link">Ignore</div></a></td>
-                    </tr>
-                </div>
-            </table>
-        </div>
-        </>
-    );
+
+
+
+
+export class Notifications extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            notificationNum: null,
+            user: "",
+            friend: "",
+            type: null
+        }
+    }
+
+    componentDidMount() {
+        axios.get(backend_url + "/notifications")
+            .then((response) => {
+                console.log(response);
+                console.log(response.data.length);
+                // console.log(response.headers.content-length);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+
+        return (
+            <div>
+               hello
+            </div>
+        );
+    }
 }
+
+export default Notifications;
+
+
+// export default function Notifications() {
+//     return (
+//         <>
+//         <header>
+//             <h1>BAD DJ ­-  Notifications</h1>
+//         </header>
+//         <div class="main">
+//             <table>
+//                 <div class="notif-odd-row">
+//                     <tr>
+//                         <td class="notif-info">Adam has sent you a friend
+//                                 request.</td>
+//                         <td><a href="#"><div class="button-link">Accept</div></a></td>
+//                         <td><a href="#"><div class="button-link">Reject</div></a></td>
+//                     </tr>
+//                 </div>
+//                 <div class="notif-even-row">
+//                     <tr>
+//                         <td class="notif-info">Della has commented on your playlist.</td>
+//                         <td><a href="#"><div class="button-link">View</div></a></td>
+//                         <td><a href="#"><div class="button-link">Ignore</div></a></td>
+//                     </tr>
+//                 </div>
+//             </table>
+//         </div>
+//         </>
+//     );
+// }
