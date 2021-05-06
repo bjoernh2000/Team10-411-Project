4f7b837cc7a386ba5628124f8a0bcf9a10c97044
bmulaw <67084560+bmulaw@users.noreply.github.com>
2021-05-05 17:57:46 -0700

    notification buttons not working

--- frontend_ver2/src/components/pages/Notifications.js
+++ frontend_ver2/src/components/pages/Notifications.js
@@ -13,7 +13,7 @@ export class Notifications extends Component {
         this.state = {
             notifications: [],
             type: [],
-            songURL: ""
+            notification_id: []
         }
     }
 
@@ -42,12 +42,24 @@ export class Notifications extends Component {
     //     })
     // }
 
+
+    onAccept(notification_id) {
+		axios.post("/notification_button_pressed", {"button": "Accept", "notification_id": notification_id})
+	}
+	onReject(notification_id) {
+		axios.post("/notification_button_pressed", {"button": "Reject", "notification_id": notification_id})
+	}
+	onDismiss(notification_id) {
+		axios.post("/notification_button_pressed", {"button": "Dismiss", "notification_id": notification_id})
+	}
+
     componentDidMount() {
         axios.get(backend_url + "/notifications")
             .then((response) => { 
                 console.log(response)               
                 this.setState({notifications: response.data.map((p) => p.text)})
                 this.setState({type: response.data.map((p) => p.type)})
+                this.setState({notification_id: response.data.map((p) => p.notification_id)})
             })
             .catch((error) => {
                 console.log(error);
@@ -67,21 +79,21 @@ export class Notifications extends Component {
                     </h2>
                 </li> )
 
-                // if (this.state.type[i] == "NOTIFICATION") {
-                //     notification_list.push(
-                //         <a href="#"><div class="button-link" onClick={this.shareSong} >View</div></a>
-                //     )
-                //     notification_list.push(
-                //         <a href="#"><div class="button-link">Ignore</div></a>
-                //     )
-                // } else if (this.state.type[i]  == "FRIEND_REQUEST") {
-                //     notification_list.push(
-                //         <a href="#"><div class="button-link">Accept</div></a>
-                //     )
-                //     notification_list.push(
-                //         <a href="#"><div class="button-link">Reject</div></a>
-                //     )
-                // }
+                if (this.state.type[i] == "NOTIFICATION") {
+                    notification_list.push(
+                        <a href="#"><div class="button-link" onClick={this.onAccept(this.state.notification_id[i])}>View</div></a>
+                    )
+                    notification_list.push(
+                        <a href="#"><div class="button-link" onClick={this.onReject(this.state.notification_id[i])}>Ignore</div></a>
+                    )
+                } else if (this.state.type[i]  == "FRIEND_REQUEST") {
+                    notification_list.push(
+                        <a href="#"><div class="button-link" >Accept</div></a>
+                    )
+                    notification_list.push(
+                        <a href="#"><div class="button-link"onClick={this.onDismiss(this.state.notification_id[i])}>Reject</div></a>
+                    )
+                }
                    
         } 
 
